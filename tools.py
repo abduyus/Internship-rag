8d9384bc8df60c463580f1e383dbe8834eca3e9a
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-06 22:30:38 +0100

    adding a basic movie bookings tool along with search movies tool for use with langchain

---
+++ tools.py
@@ -0,0 +1,28 @@
+from langchain_core.tools import tool
+
+from search import search_movies as search_movies_backend
+
+
+def movie_booking_tool(movie_title, time):
+    return f'Booking Confirmed for {movie_title} at {time}'
+
+
+def search_movies_tool(query: str):
+    results = search_movies_backend(query)
+    return [
+        {
+            "title": r.payload["title"],
+            "overview": r.payload["overview"]
+        }
+        for r in results[:5]
+    ]
+
+@tool
+def search_movies(query: str):
+    """Search for movies using semantic search"""
+    return search_movies_tool(query)
+
+@tool
+def book_movie(movie_title: str, time: str):
+    """Book a movie viewing at a given time"""
+    return movie_booking_tool(movie_title, time)
