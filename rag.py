02bfb6b7fb5ceb6a1cbf12ccfdc5ce279a8e067c
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-06 22:29:38 +0100

    receiving the users prompt and retrieving the results from the database to build a prompt

---
+++ rag.py
@@ -0,0 +1,65 @@
+from search import search_movies
+import ollama
+
+question = input('Ask me about movies: ')
+
+results = search_movies(question)
+
+context = ""
+
+# Iterating over the movies to add their details to the context for the LLM prompt
+for movie in results:
+    payload = movie.payload
+
+    context += f"""
+    --------------------------------------------
+    --------------------------------------------
+Title: {payload['title']}
+Genres: {payload['genres']}
+Director: {payload['director']}
+Overview:
+{payload['overview']}
+
+
+"""
+
+
+# print(context)
+
+
+# Building the LLM prompt using the user's question and the retrieved context
+prompt = f"""
+YYou are a movie recommendation assistant.
+
+You MUST follow these rules:
+- Only use information from the retrieved movies below.
+- Do NOT use external knowledge or prior training.
+- Do NOT invent plot details or facts.
+- Only describe movies using the provided overviews.
+- If information is missing, say "Not available in retrieved data."
+
+Your task:
+Recommend movies that match the user's request.
+
+Retrieved Movies:
+{context}
+
+User Question:
+{question}
+
+Answer in a clear, structured way:
+- Recommended movies
+- Why they match the request (ONLY using retrieved overviews)
+"""
+
+response = ollama.chat(
+    model="qwen2.5:14b",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+
+print(response["message"]["content"])
