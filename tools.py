0848f966068f91ad80915ba3f6aad968fb55c7f1
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-07 09:13:09 +0100

    updating parameters for functions and tools

--- tools.py
+++ tools.py
@@ -7,7 +7,7 @@ def movie_booking_tool(movie_title, time):
     return f'Booking Confirmed for {movie_title} at {time}'
 
 
-def search_movies_tool(query: str):
+def search_movies_tool(query):
     results = search_movies_backend(query)
     return [
         {
@@ -18,11 +18,11 @@ def search_movies_tool(query: str):
     ]
 
 @tool
-def search_movies(query: str):
+def search_movies(query):
     """Search for movies using semantic search"""
     return search_movies_tool(query)
 
 @tool
-def book_movie(movie_title: str, time: str):
+def book_movie(movie_title, time):
     """Book a movie viewing at a given time"""
     return movie_booking_tool(movie_title, time)
