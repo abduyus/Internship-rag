636f94e9d19fd24ebc9a4d287b12fa5fd613efe5
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-07 12:15:15 +0100

    refactoring the dedup and rerank into separate functions

--- search.py
+++ search.py
@@ -4,15 +4,53 @@ from dotenv import load_dotenv
 from qdrant_client import QdrantClient
 from sentence_transformers import SentenceTransformer, CrossEncoder
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
-
 # Load environment variables
 load_dotenv()
+
+model = SentenceTransformer(os.getenv('EMBEDDING_MODEL'))
+reranker = CrossEncoder(os.getenv('RERANKER_MODEL'))
+
+
+
+def deduplicate_results(points):
+    """Remove duplicate points from a list"""
+    # Remove duplicate results by title
+    seen = set()
+    deduped = []
+
+    for point in points:
+        title = point.payload["title"]
+
+        if title not in seen:
+            deduped.append(point)
+            seen.add(title)
+    return deduped
+
+
+def rerank_results(question, points, limit=5):
+    """Rerank results from a list"""
+    pairs = [
+        (question, point.payload["overview"])
+        for point in points
+    ]
+
+    # Rerank results with a cross-encoder
+    scores = reranker.predict(pairs)
+
+    # Sort reranked results by score
+    reranked = sorted(
+        zip(points, scores),
+        key=lambda x: x[1],
+        reverse=True
+    )
+
+    # Return the top 5 most relevant movies
+    return [point for point, _ in reranked[:limit]]
 
 
 # Creating a reusable function to search movies in the qdrant collection
 def search_movies(question, limit=15):
+    """Retrieve, deduplicate, and rerank movies relevant to the user's query"""
     qdrant_client = QdrantClient(
         url=os.getenv('QDRANT_URL'),
         api_key=os.getenv('QDRANT_API_KEY'),
@@ -26,33 +64,9 @@ def search_movies(question, limit=15):
     results = qdrant_client.query_points(collection_name='movies', query=query_vector, limit=limit)
 
     # Remove duplicate results by title
-    seen = set()
-    deduped = []
+    deduped = deduplicate_results(results.points)
 
-    for point in results.points:
-        title = point.payload["title"]
-
-        if title not in seen:
-            deduped.append(point)
-            seen.add(title)
-
-    # Reranking
-    pairs = [
-        (question, point.payload["overview"])
-        for point in deduped
-    ]
-
-    # Rerank results with a cross-encoder
-    scores = reranker.predict(pairs)
-
-    # Sort reranked results by score
-    reranked = sorted(
-        zip(results.points, scores),
-        key=lambda x: x[1],
-        reverse=True
-    )
-
-    # Return the top 5 most relevant movies
-    top_results = [p[0] for p in reranked[:5]]
+    # Rerank results with a cross encoder
+    top_results = rerank_results(question, deduped, limit=5)
 
     return top_results
