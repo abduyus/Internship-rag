e1a17d5ca6112ba7c6aad58eb32933984a022ea0
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-07 13:34:43 +0100

    add rerank_results function and corresponding test cases

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,4 +1,4 @@
-from search import deduplicate_results
+from search import deduplicate_results, rerank_results
 
 
 class MockPoint:
@@ -35,3 +35,33 @@ def test_deduplicate_removes_duplicate_title():
     ]
 
     assert titles == ['Iron Man', 'Spiderman']
+
+
+class RerankMockPoint:
+    def __init__(self, title, overview):
+        self.payload = {
+            "title": title,
+            "overview": overview
+        }
+
+
+def test_rerank_results(monkeypatch):
+    points = [
+        RerankMockPoint('Iron Man', 'overview A'),
+        RerankMockPoint('Spiderman', 'overview B'),
+        RerankMockPoint('Jamal Khan', 'overview C'),
+    ]
+
+    class FakeReranker:
+        def predict(self, _):
+            return [0.1, 0.9, 0.5]
+
+    monkeypatch.setattr('search.CrossEncoder', lambda *args, **kwargs: FakeReranker())
+    result = rerank_results('funny superhero movie', points)
+
+    titles = [
+        movie.payload['title']
+        for movie in result
+    ]
+
+    assert titles == ['Spiderman', 'Jamal Khan', 'Iron Man']
