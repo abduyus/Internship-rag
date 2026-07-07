1bcb9fd7a51b28394829b80b958e2c06a0e2f03d
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-07 16:59:47 +0100

    Adding more specific interation tests for the search_movies function

--- tests/test_search_integration.py
+++ tests/test_search_integration.py
@@ -1,5 +1,23 @@
 from search import search_movies
 
+
 def test_search_movies_returns_results():
     results = search_movies('tony stark')
     assert len(results) > 0
+
+
+def test_search_movies_returns_movie_payload():
+    results = search_movies('space exploration')
+
+    movie = results[0]
+
+    assert 'title' in movie.payload
+    assert 'overview' in movie.payload
+
+
+def test_search_movie_finds_iron_man():
+    results = search_movies('movie featuring tony stark')
+
+    titles = [movie.payload['title'] for movie in results]
+
+    assert 'Iron Man' in titles
