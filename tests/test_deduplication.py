24e4d41e02504e72a716cbf8efd63182c07b09e5
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-07 15:20:43 +0100

    refactoring the tests to be in different files for dedup and reranking

---
+++ tests/test_deduplication.py
@@ -0,0 +1,43 @@
+from search import deduplicate_results
+
+
+class MockPoint:
+    def __init__(self, title):
+        self.payload = {
+            "title": title
+        }
+
+
+def test_deduplicate_results():
+    points = [
+        MockPoint('Iron Man'),
+        MockPoint('Iron Man'),
+        MockPoint('Spiderman'),
+    ]
+
+    result = deduplicate_results(points)
+
+    assert len(result) == 2
+
+
+def test_deduplicate_removes_duplicate_title():
+    points = [
+        MockPoint('Iron Man'),
+        MockPoint('Iron Man'),
+        MockPoint('Spiderman'),
+    ]
+
+    result = deduplicate_results(points)
+
+    titles = [
+        movie.payload['title']
+        for movie in result
+    ]
+
+    assert titles == ['Iron Man', 'Spiderman']
+
+
+def test_deduplicate_empty_results():
+    points = []
+    result = deduplicate_results(points)
+    assert result == []
