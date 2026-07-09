46fb88c2d7e44ece180d6dfb3cb00fe32ff05ae1
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-09 09:49:55 +0100

    add CLI argument support for user input in agent

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
@@ -13,16 +14,21 @@ load_dotenv()
 llm = ChatOllama(model=os.getenv('OLLAMA_MODEL'))
 tools = [search_movies, book_movie]
 agent = create_agent(llm, tools)
-message = input('Ask me about movie details and bookings: ')
 
-# Send the user input to the agent 
+# Allowing the user input through CLI arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--message', '-m', help='User message')
+args = parser.parse_args()
+message = args.message or os.getenv('MESSAGE') or input('Ask me about movie details and bookings: ')
+
+# Send the user input to the agent
 response = agent.invoke({
     "messages": [
         ("user", message)
     ]
 })
 
-# Selecting the final response received from the LLM 
+# Selecting the final response received from the LLM
 final_message = next(
     (msg for msg in reversed(response['messages']) if isinstance(msg, AIMessage)),
     None
