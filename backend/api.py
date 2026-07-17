dc5d74a2e6db459e9717dbf414c65026790fe247
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-17 11:36:50 +0100

    adding CORS middleware

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from backend.agent import ask_movie_agent
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ChatRequest(BaseModel):
