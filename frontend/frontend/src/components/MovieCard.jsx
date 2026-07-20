2eb4e0c94a8aed099c45b5e60a791c95f99729db
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-20 16:41:46 +0100

    styling image, genres, title, and overview

--- frontend/frontend/src/components/MovieCard.jsx
+++ frontend/frontend/src/components/MovieCard.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const StyledMovieCard = styled.div`
     background-color: var(--color-grey-100);
-    margin: 1.6rem 0.8rem;
-    padding: 1.8rem;
+    //margin: 1.6rem 0.8rem;
+    //padding: 1.8rem;
     border-radius: var(--border-radius-lg);
     display: flex;
     flex-direction: column;
@@ -11,44 +11,174 @@ const StyledMovieCard = styled.div`
     gap: 1.8rem;
     box-shadow: var(--shadow-md);
     transition: all 0.3s;
+    grid-row: 2;
+    //height: 100%;
+    padding-bottom: 1.8rem;
     
     
 
     &:hover {
         box-shadow: var(--shadow-lg);
         
-        margin: 2.4rem 0.8rem;
+    }
+    
+    .movie_title {
+        margin-left: 1.8rem;
+    }
+    
+    .overview {
+        margin-left: 1.8rem;
     }
     
     
-    > p:nth-of-type(1) {
-        grid-column: 1;
-    }
-    > p:nth-of-type(3) {
-        grid-column: 2;
+   .movie_image {
+       width: 100%;
+       border-top-left-radius: var(--border-radius-lg);
+       border-top-right-radius: var(--border-radius-lg);
+   }
+    
+    .matches_reason {
+        padding-left: 1.8rem;
     }
 
-    p:first-of-type + ul{
+    .card__genres {
         display: flex;
-        gap: 1.8rem;
-        margin-bottom: 1.2rem;
+        flex-wrap: wrap;
+        gap: 1.2rem;
+        font-size: 1.3rem;
+        margin-bottom: 2.4rem;
+        margin-top: 1.2rem;
+        margin-left: 1.8rem;
     }
-    
-    li {
-        //background-color: var(--color-grey-600);
-    }
-    > p:first-of-type + ul li{
-         background-color: var(--color-brand-600);
-         padding: 0.4rem 1rem;
-         border-radius: var(--border-radius-lg);
+
+    .card__genre {
+        display: inline-block;
+        padding: 0.4rem 0.8rem;
         font-size: 1.2rem;
         text-transform: uppercase;
-     }
-    
-    ul:last-of-type {
-        display: flex;
-        flex-direction: column;
-        gap: 1.2rem
+        color: #333;
+        border-radius: 100px;
+        font-weight: 600;
+    }
+
+    .card__genre.action {
+        background-color: #f8d7da;
+        color: #721c24;
+    }
+
+    .card__genre.science {
+        background-color: #d4edda;
+        color: #155724;
+    }
+
+    .card__genre.fiction {
+        background-color: #fff3cd;
+        color: #856404;
+    }
+
+    .card__genre.adventure {
+        background-color: #cce5ff;
+        color: #004085;
+    }
+
+    .card__genre.comedy {
+        background-color: #d1ecf1;
+        color: #0c5460;
+    }
+
+    .card__genre.animation {
+        background-color: #f0abfc;
+        color: #701a75;
+    }
+
+    .card__genre.crime {
+        background-color: #d6d8d9;
+        color: #6c757d;
+    }
+
+    .card__genre.documentary {
+        background-color: #fca5a5;
+        color: #7c2d12;
+    }
+
+    .card__genre.drama {
+        background-color: #d9f99d;
+        color: #365314;
+    }
+
+    .card__genre.family {
+        background-color: #f8d7da;
+        color: #721c24;
+    }
+
+    .card__genre.fantasy {
+        background-color: #d4edda;
+        color: #155724;
+    }
+
+    .card__genre.fiction {
+        background-color: #fff3cd;
+        color: #856404;
+    }
+
+    .card__genre.foreign {
+        background-color: #cce5ff;
+        color: #004085;
+    }
+
+    .card__genre.history {
+        background-color: #d1ecf1;
+        color: #0c5460;
+    }
+
+    .card__genre.horror {
+        background-color: #f0abfc;
+        color: #701a75;
+    }
+
+    .card__genre.movie {
+        background-color: #d6d8d9;
+        color: #6c757d;
+    }
+
+    .card__genre.music {
+        background-color: #fca5a5;
+        color: #7c2d12;
+    }
+
+    .card__genre.mystery {
+        background-color: #d9f99d;
+        color: #365314;
+    }
+
+    .card__genre.romance {
+        background-color: #f8d7da;
+        color: #721c24;
+    }
+
+    .card__genre.science {
+        background-color: #d4edda;
+        color: #155724;
+    }
+
+    .card__genre.thriller {
+        background-color: #fff3cd;
+        color: #856404;
+    }
+
+    .card__genre.tv {
+        background-color: #cce5ff;
+        color: #004085;
+    }
+
+    .card__genre.war {
+        background-color: #d1ecf1;
+        color: #0c5460;
+    }
+
+    .card__genre.western {
+        background-color: #f0abfc;
+        color: #701a75;
     }
     
 `
@@ -58,6 +188,21 @@ function MovieCard({movie}) {
 
     return (
         <StyledMovieCard>
+            <img src={'img_1.png'} alt="Movie Image" className={'movie_image'} />
+            <div className="card__genres">
+                {genres.map((genre) => {
+                    return (
+                        <span className={'card__genre ' + genre.toLowerCase()}>{genre}</span>
+                    )
+                })}
+            </div>
+            <h1 className={'movie_title'}>{title} ({year})</h1>
+            <p className={'overview'}>{overview}</p>
+            <p className={'matches_reason'}>{why_it_matches}</p>
+
+
+
+
         </StyledMovieCard>
     );
 }
