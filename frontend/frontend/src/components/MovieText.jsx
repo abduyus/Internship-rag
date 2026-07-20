abb0cb9270e0770bc6952d3a8b3dc6ae70786798
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-20 16:42:12 +0100

    spanning the movie text to across the grid

--- frontend/frontend/src/components/MovieText.jsx
+++ frontend/frontend/src/components/MovieText.jsx
@@ -2,9 +2,11 @@ import styled from "styled-components";
 
 const StyledMovieText = styled.h1`
      
-        margin-bottom: 1rem;
+       
         text-align: center;
         font-size: 1.8rem;
+        grid-row: 1;
+    grid-column: span 4;
     
 `
 
