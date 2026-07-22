0f3597f539d3b6e1dde3ef8de0e77376ccc80bbc
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-07-22 14:44:59 +0100

    changing data

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -3,49 +3,63 @@ import RecommendationPanel from "./sections/RecommendationPanel.jsx";
 import {useState} from "react";
 
 
-// const MOCK_RECOMMENDATION =
-//     {
-//         "summary": "Two superhero movies with comedic elements are suggested: 'Super' (2010) and 'All Superheroes Must Die' (2011). Both have a funny aspect, but 'Super' is more directly focused on humor.",
-//         "movies": [
-//             {
-//                 "title": "Super",
-//                 "year": 2010,
-//                 "genres": [
-//                     "Comedy",
-//                     "Action",
-//                     "Drama"
-//                 ],
-//                 "overview": "After his wife falls under the influence of a drug dealer, an everyday guy transforms himself into Crimson Bolt, a superhero with the best intentions, though he lacks for heroic skills.",
-//                 "why_it_matches": [
-//                     "The movie focuses on humor and features an everyday man turning into a superhero despite lacking heroic abilities."
-//                 ],
-//                 "match_score": 0.85,
-//                 "backdrop_url": "https://image.tmdb.org/t/p/original/5OGrBmi0fWAE6b1R6uv5EnuWOaL.jpg"
-//             },
-//             {
-//                 "title": "All Superheroes Must Die",
-//                 "year": 2011,
-//                 "genres": [
-//                     "Science Fiction",
-//                     "Thriller"
-//                 ],
-//                 "overview": "Masked vigilantes are rendered powerless by their archenemy and are forced to complete deadly tasks in order to save the lives of more than 100 innocent civilians.",
-//                 "why_it_matches": [
-//                     "This movie includes a comedic element but has a darker, thriller aspect, making it less focused on humor compared to 'Super'."
-//                 ],
-//                 "match_score": 0.75,
-//                 "backdrop_url": "https://image.tmdb.org/t/p/original/vkniCN8rQjpItnP6Pq7K4aAUxok.jpg"
-//             }
-//         ]
-//     };
+const MOCK_RECOMMENDATION =
+    {
+        "summary": "",
+        "movies": [
+            {
+                "title": "Iron Man",
+                "year": 2008,
+                "genres": [],
+                "overview": "Tony Stark creates an advanced suit of armor and uses it to fight crime.",
+                "why_it_matches": [
+                    "Starring Tony Stark",
+                    "Advanced technology themes"
+                ],
+                "match_score": 9.573412470645808,
+                "rating": 7.9,
+                "duration": 126,
+                "backdrop_url": "https://image.tmdb.org/t/p/original/cyecB7godJ6kNHGONFjUyVN9OX5.jpg"
+            },
+            {
+                "title": "Iron Man 2",
+                "year": 2010,
+                "genres": [],
+                "overview": "Tony Stark builds an even more advanced suit of armor and faces new threats.",
+                "why_it_matches": [
+                    "Starring Tony Stark",
+                    "Advanced technology themes"
+                ],
+                "match_score": 9.37846512245098,
+                "rating": 7.7,
+                "duration": 121,
+                "backdrop_url": "https://image.tmdb.org/t/p/original/7lmBufEG7P7Y1HClYK3gCxYrkgS.jpg"
+            },
+            {
+                "title": "Iron Man 3",
+                "year": 2013,
+                "genres": [],
+                "overview": "Tony Stark tries to keep his superhero identity a secret while facing a new enemy.",
+                "why_it_matches": [
+                    "Starring Tony Stark",
+                    "Advanced technology themes"
+                ],
+                "match_score": 9.165207434798884,
+                "rating": 7.7,
+                "duration": 132,
+                "backdrop_url": "https://image.tmdb.org/t/p/original/iVped1djsF0tvGkvnHbzsE3ZPTF.jpg"
+            }
+        ]
+    };
 
 
 
 function App() {
-    const [recommendation, setRecommendation] = useState({
-        summary: "Intelligent movie recommendation and booking system using RAG + LangChain",
-        movies: [],
-    });    // const [recommendation, setRecommendation] = useState(MOCK_RECOMMENDATION);
+    // const [recommendation, setRecommendation] = useState({
+    //     summary: "Intelligent movie recommendation and booking system using RAG + LangChain",
+    //     movies: [],
+    // });
+    const [recommendation, setRecommendation] = useState(MOCK_RECOMMENDATION);
     const [isLoading, setIsLoading] = useState(false)
 
 
